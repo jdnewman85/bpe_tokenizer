fc90ef91539f195c5ed4541c0db8df7d2ab4716c
James Newman <jdnewman85@gmail.com>
2022-12-26 14:55:22 -0700

    Tidy

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use nom::{
-    IResult,
     branch::alt,
     bytes::complete::{tag, take_while1},
-    combinator::{opt, recognize}, multi::many1, character::complete::char, sequence::pair, error::ParseError, InputIter, AsChar,
+    character::complete::char,
+    combinator::{opt, recognize},
+    error::ParseError,
+    multi::many1,
+    sequence::pair,
+    AsChar, IResult, InputIter,
 };
 use nom_unicode;
 
@@ -20,57 +24,48 @@ fn contraction(input: &str) -> IResult<&str, &str> {
 
 fn opt_preceding_space<I, O, E: ParseError<I>, F>(f: F) -> impl FnMut(I) -> IResult<I, I, E>
 where
-    I: Clone + nom::Slice<std::ops::RangeTo<usize>> + nom::Offset + nom::InputIter + nom::Slice<std::ops::RangeFrom<usize>>,
-    F: nom::Parser<I, O, E>, <I as InputIter>::Item: AsChar
+    I: Clone
+        + nom::InputIter
+        + nom::Offset
+        + nom::Slice<std::ops::RangeFrom<usize>>
+        + nom::Slice<std::ops::RangeTo<usize>>,
+    F: nom::Parser<I, O, E>, <I as InputIter>::Item: AsChar,
 {
-    recognize(
-        pair(
-            opt(char(' ')),
-            f,
-        )
-    )
+    recognize(pair(opt(char(' ')), f))
 }
 
-fn space_contraction(input: &str) -> IResult<&str, &str> {
-    opt_preceding_space(
-        contraction
-    )(input)
+fn opt_space_contraction(input: &str) -> IResult<&str, &str> {
+    opt_preceding_space(contraction)(input)
 }
 
-fn space_unicode_alpha1(input: &str) -> IResult<&str, &str> {
-    opt_preceding_space(
-        nom_unicode::complete::alpha1,
-    )(input)
+fn opt_space_unicode_alpha1(input: &str) -> IResult<&str, &str> {
+    opt_preceding_space(nom_unicode::complete::alpha1)(input)
 }
 
-fn space_unicode_digit1(input: &str) -> IResult<&str, &str> {
-    opt_preceding_space(
-        nom_unicode::complete::digit1,
-    )(input)
+fn opt_space_unicode_digit1(input: &str) -> IResult<&str, &str> {
+    opt_preceding_space(nom_unicode::complete::digit1)(input)
 }
 
 fn is_non_unicode_space_alphanumeric(chr: char) -> bool {
     !(nom_unicode::is_alphanumeric(chr) | nom_unicode::is_whitespace(chr))
 }
 
-fn space_non_unicode_alphanumeric1(input: &str) -> IResult<&str, &str> {
-    opt_preceding_space(
-        take_while1(is_non_unicode_space_alphanumeric)
-    )(input)
+fn opt_space_non_unicode_alphanumeric1(input: &str) -> IResult<&str, &str> {
+    opt_preceding_space(take_while1(is_non_unicode_space_alphanumeric))(input)
 }
 
 fn pat(input: &str) -> IResult<&str, Vec<&str>> {
-    many1(
-        alt((
-            space_contraction,
-            space_unicode_alpha1,
-            space_unicode_digit1,
-            space_non_unicode_alphanumeric1,
-            nom_unicode::complete::space1,
-        ))
-    )(input)
+//TODO: Should this slit on and consume newlines? I don't think so?
+    many1(alt((
+        opt_space_contraction,
+        opt_space_unicode_alpha1,
+        opt_space_unicode_digit1,
+        opt_space_non_unicode_alphanumeric1,
+        /* Original regex has spaces before a non-space here, but that seems unneeded as it'd be
+        * caught by the space1, then one of the above on next match itteration? */
+        nom_unicode::complete::space1,
+    )))(input)
 }
-
 
 fn main() {
     //println!("{:?}", pat("This is a test! y'all's allright?").unwrap());
