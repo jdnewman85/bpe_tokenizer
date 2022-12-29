da4f8c0128d00354705f9e768ac717d7970d5587
James Newman <jdnewman85@gmail.com>
2022-12-28 23:36:31 -0700

    Start replicating bpe functions. Not sure if exactly right, or if that matters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while1},
@@ -67,7 +69,90 @@ fn pat(input: &str) -> IResult<&str, Vec<&str>> {
     )))(input)
 }
 
+/* python
+def bytes_to_unicode():
+    """
+    Returns list of utf-8 byte and a corresponding list of unicode strings.
+    The reversible bpe codes work on unicode strings.
+    This means you need a large # of unicode characters in your vocab if you want to avoid UNKs.
+    When you're at something like a 10B token dataset you end up needing around 5K for decent coverage.
+    This is a signficant percentage of your normal, say, 32K bpe vocab.
+    To avoid that, we want lookup tables between utf-8 bytes and unicode strings.
+    And avoids mapping to whitespace/control characters the bpe code barfs on.
+    """
+    bs = list(range(ord("!"), ord("~")+1))+list(range(ord("¡"), ord("¬")+1))+list(range(ord("®"), ord("ÿ")+1))
+    cs = bs[:]
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            bs.append(b)
+            cs.append(2**8+n)
+            n += 1
+    cs = [chr(n) for n in cs]
+    return dict(zip(bs, cs))
+*/
+
+fn is_valid_bpe_char<T>(i: T) -> bool
+where T: nom::AsChar + Copy //TODO Or nom_unicode::IsChar?
+{
+    match i.as_char() {
+        '!'..='~' |
+        '¡'..='¬' |
+        '®'..='ÿ' => true,
+        _ => false,
+    }
+}
+
+fn create_bpe_char_encoder<T>() -> HashMap<T, char>
+where T: nom::AsChar + Copy + std::hash::Hash + std::cmp::Eq + std::convert::From<u8> + std::fmt::Debug //TODO Or nom_unicode::IsChar?
+{
+    let mut map: HashMap<T, char> = HashMap::new();
+
+    let n: u32 = 0; //Current offset
+    for i in 0..=255 {
+        let is_valid = is_valid_bpe_char(i);
+        let cs: char = match is_valid {
+            true => i.into(),
+            false => char::from_u32(256 + n).unwrap(),
+        };
+
+        map.insert(i.into(), cs);
+    }
+
+    dbg!(&map);
+    return map
+}
+
+fn create_bpe_char_decoder<T>() -> HashMap<char, T>
+where T: nom::AsChar + Copy + std::hash::Hash + std::cmp::Eq + std::convert::From<u8> + std::fmt::Debug //TODO Or nom_unicode::IsChar?
+{
+    //TODO: Use create_bpe_char_encoder, and swap key/values?
+    let mut map: HashMap<char, T> = HashMap::new();
+
+    let n: u32 = 0; //Current offset
+    for i in 0..=255 {
+        let is_valid = is_valid_bpe_char(i);
+        let cs: char = match is_valid {
+            true => i.into(),
+            false => char::from_u32(256 + n).unwrap(),
+        };
+
+        map.insert(cs, i.into());
+    }
+
+    dbg!(&map);
+
+    return map
+}
+
+
 fn main() {
     //println!("{:?}", pat("This is a test! y'all's allright?").unwrap());
     dbg!(pat("This is a test! y'all's allright?\nDo newlines work?!%? 1535").unwrap());
+
+    let bpe_char_encoder = create_bpe_char_encoder::<u8>();
+    dbg!(bpe_char_encoder[&('A' as u8)]);
+
+    let bpe_char_encoder = create_bpe_char_encoder::<char>();
+    dbg!(bpe_char_encoder[&'!']);
 }
