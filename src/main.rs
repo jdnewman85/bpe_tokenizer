fd9cbc96af9f7eb3df05c7ec2c7a6b988c0b2bd3
James Newman <jdnewman85@gmail.com>
2023-01-01 01:27:45 -0700

    Use clap to take input, and model files from program args.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 mod tokenizer;
+use std::path::PathBuf;
+
 use tokenizer::Tokenizer;
 
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    input: Option<String>,
+
+    #[arg(short, long, value_name = "default: encoder.json")]
+    encoder_filename: Option<PathBuf>,
+    #[arg(short, long, value_name = "default: vocab.bpe")]
+    vocab_filename: Option<PathBuf>,
+}
+
 fn main() {
-    let my_tokenizer: Tokenizer<char> = Tokenizer::new("encoder.json", "vocab.bpe");
-    let test_text = "This is a test! y'all's alright?\nDo newlines work?!%? 1535";
-    let tokens = my_tokenizer.tokenize(test_text);
-    dbg!(&tokens);
-    dbg!(&tokens.len());
-    let text = my_tokenizer.detokenize(tokens);
-    dbg!(&text);
-    assert!(&text == &test_text);
+    let cli = Args::parse();
+
+    let encoder_filename = cli.encoder_filename.unwrap_or("encoder.json".into());
+    let vocab_filename = cli.vocab_filename.unwrap_or("vocab.bpe".into());
+
+    if let Some(input) = cli.input {
+        let my_tokenizer: Tokenizer<char> = Tokenizer::new(encoder_filename, vocab_filename);
+        let tokens = my_tokenizer.tokenize(&input);
+        dbg!(&tokens);
+        dbg!(&tokens.len());
+
+        //Sanity check TODO Remove?
+        let text = my_tokenizer.detokenize(tokens);
+    //    dbg!(&text);
+        assert!(&text == &input);
+    } else {
+        println!("TODO: Process from stdin");
+    }
+
 }
